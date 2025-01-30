5bc6dc7e6dc1cae2cbcefb9b1ac8ca492f99b454
Dhatri78 <120077917+Dhatri78@users.noreply.github.com>
2025-01-30 11:11:45 +0530

    Add files via upload

---
+++ sqr.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class sqr {
+    public static void main(String args[]) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.print(i * i + " ");
+            i++;
+        }
+    }
+}
