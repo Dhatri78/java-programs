8a94983103cbfff499e9631b8275df2dfd8457bf
Dhatri78 <120077917+Dhatri78@users.noreply.github.com>
2025-02-07 22:41:20 +0530

    Add files via upload

---
+++ withoutspace.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public static void main() {
+    String a = scn.nextLine();
+    String b = scn.nextLine();
+    // Write your code below that prints ab
+    System.out.println(a + b);
+}
