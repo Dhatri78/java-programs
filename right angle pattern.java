2b3a75d383422f14094f77f1979760943ec7170d
Dhatri78 <120077917+Dhatri78@users.noreply.github.com>
2025-02-10 12:04:38 +0530

    Add files via upload

---
+++ right angle pattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class rightanglepattern {
+    public static void main(String args[]) {
+        Scanner sc = new scanner(System.in);
+        int n = sc.nextInt;
+        for (int j = 1; j <= i; j++) {
+            if (j == 1 || j == i || i == rows) {
+                System.out.print("* ");
+            } else {
+                System.out.print("  ");
+            }
+        }
+
+        System.out.println();
+    }
+}
---
+++ withspace - Copy.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class withspace {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        // Print in the required format and a new line at the end
+        System.out.print(a + " ");
+        System.out.print(b);
+    }
+}
