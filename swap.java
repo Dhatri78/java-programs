2a5fdd668a055461221351acb6f227f4324abacb
Dhatri78 <120077917+Dhatri78@users.noreply.github.com>
2025-02-18 16:44:51 +0530

    Add files via upload

--- swap.java
+++ swap.java
@@ -1,12 +1,15 @@
-
 import java.util.*;
+import java.io;
 
 public class swap {
-    public static void swap(int a, int b) {
-        // code here
-        int t = a;
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        // Write Code to Swap
+        int temp = a;
         a = b;
-        b = t;
+        b = temp;
         System.out.println(a + " " + b);
     }
 }
