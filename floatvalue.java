e2c197756d1cd75827f42379000d694d99eff17c
Dhatri78 <120077917+Dhatri78@users.noreply.github.com>
2025-02-19 12:09:11 +0530

    Add files via upload

---
+++ floatvalue.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class floatvalue {
+    public static void main(String args[]) {
+        // Take a Float input and Print
+        Scanner sc = new Scanner(System.in);
+        float n = sc.nextFloat();
+        System.out.println(n * 10);
+    }
+}
